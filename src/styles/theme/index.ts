d59a8f4f4390debfff6a4baa171f2a9703b66e05
hanseulhee <3021062@gmail.com>
2023-09-12 01:12:36 +0900

    [#1] apply emotion, framer-motion styles

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,14 @@
+const theme = {
+  color: {
+    white: "#ffffff",
+    black: "#000000",
+    gray: "#999A9A",
+  },
+  fontWeight: {
+    light: 300,
+    normal: 500,
+    bold: 700,
+  },
+};
+
+export default theme;
