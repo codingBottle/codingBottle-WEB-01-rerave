f6702a2279a408a94513a2902a3c6a56640f3b23
“Dami-LEE00” <dxmxxii@gmail.com>
2023-09-28 05:24:29 +0900

    [#10] add font, ul, li, a styles

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -7,6 +7,9 @@ function GlobalStyle() {
 export default GlobalStyle;
 
 const globalStyle = (theme: Theme) => css`
+  @import url('https://fonts.googleapis.com/css2?family=Marcellus&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Marcellus&family=Noto+Sans+KR:wght@200;300;400;500;600;700;800&display=swap');
+
   * {
     margin: 0;
     padding: 0;
@@ -18,5 +21,14 @@ const globalStyle = (theme: Theme) => css`
   body {
     background-color: ${theme.color.white};
     color: ${theme.color.black};
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
   }
 `;
