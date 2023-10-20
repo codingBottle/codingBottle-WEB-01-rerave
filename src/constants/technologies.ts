2aaff1cebde8ca66a466ef51366b0a2602b01b19
“Dami-LEE00” <dxmxxii@gmail.com>
2023-10-20 21:14:00 +0900

    [#15] create technology card info

---
+++ src/constants/technologies.ts
@@ -0,0 +1,24 @@
+import panellagen from "../assets/images/technology/tech_penellagen.jpg";
+import sheet from "../assets/images/technology/tech_sheet.jpg";
+import ingredient from "../assets/images/technology/tech_ingredient.jpg";
+
+export const techArr = [
+  {
+    img: panellagen,
+    title: "01. PENELLAGEN®",
+    content:
+      "페넬라겐(Penellagen) 은 브랜드의 핵심 기술 요소인 피부 침투(Penetration)기술과 콜라겐(collagen)의 합성어로서 자체 연구・개발한기술력에 대한 자신감을 담고 있습니다.",
+  },
+  {
+    img: sheet,
+    title: "02. Triple Locking Sheet",
+    content:
+      "확실하게 잠궈주고, 확실하게 올려주고, 확실하게 살려주는 트리플-락킹(triple-locking) 시트. 뛰어난 밀착력으로 유효성분 증발은 차단하고 피부는 편하게 숨쉴수 있는 리라브 독점 시트입니다.",
+  },
+  {
+    img: ingredient,
+    title: "03. Ingredient",
+    content:
+      "죽지않는 불가사리 재생의 원천 성분으로써 항산화성, 미백효과와 주름 억제능력이 매우 뛰어나고 독성과 피부자극이 없어 믿고 사용할 수 있는 안전한 성분입니다.",
+  },
+];
